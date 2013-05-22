9526e80384f58cc36f3f263a6aa0bac7506faa11
Dreyfus Cécile <cecile.dreyfus--laquieze@epfedu.fr>
2013-05-22 11:19:10 +0200

    Code Cécile

--- Ouroboros/gen/epf/domethic/ouroboros/R.java
+++ Ouroboros/gen/epf/domethic/ouroboros/R.java
@@ -26,55 +26,80 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int connexion_flechon=0x7f020000;
-        public static final int custom_btn_breaker_bay=0x7f020001;
-        public static final int fond_connexion=0x7f020002;
-        public static final int gd_logo_transparent=0x7f020003;
-        public static final int ic_contact_picture=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
-        public static final int ic_menu_search=0x7f020006;
-        public static final int uroboroslogoseul=0x7f020007;
+        public static final int archives=0x7f020000;
+        public static final int bouton_connexion=0x7f020001;
+        public static final int codification=0x7f020002;
+        public static final int custom_btn_breaker_bay=0x7f020003;
+        public static final int encadre=0x7f020004;
+        public static final int fond=0x7f020005;
+        public static final int grand_logo=0x7f020006;
+        public static final int hospi_en_cours=0x7f020007;
+        public static final int ic_contact_picture=0x7f020008;
+        public static final int ic_launcher=0x7f020009;
+        public static final int ic_menu_search=0x7f02000a;
+        public static final int infos_g=0x7f02000b;
+        public static final int uroboroslogoseul=0x7f02000c;
     }
     public static final class id {
-        public static final int ACodifier=0x7f0a000e;
-        public static final int AjoutDocument=0x7f0a000c;
-        public static final int Archives=0x7f0a0010;
-        public static final int AutresActions=0x7f0a000f;
-        public static final int CreationDossier=0x7f0a000b;
-        public static final int Deconnexion=0x7f0a0007;
-        public static final int GererVosDossier=0x7f0a000a;
-        public static final int Hospitalisation=0x7f0a0009;
-        public static final int HostAnd=0x7f0a0012;
-        public static final int MonCompte=0x7f0a0011;
-        public static final int NomMedecin=0x7f0a0006;
-        public static final int Transfert=0x7f0a000d;
-        public static final int TrouverPatient=0x7f0a0008;
-        public static final int action_recherche=0x7f0a001a;
-        public static final int action_settings=0x7f0a0019;
-        public static final int editNom=0x7f0a0001;
-        public static final int editPswd=0x7f0a0000;
-        public static final int imageButton1=0x7f0a0003;
-        public static final int imageView1=0x7f0a0002;
-        public static final int linearLayout1=0x7f0a0004;
-        public static final int linearLayout3=0x7f0a0014;
-        public static final int linearLayout5=0x7f0a0016;
-        public static final int list=0x7f0a0017;
-        public static final int saveLife=0x7f0a0013;
-        public static final int slideMenu=0x7f0a0005;
-        public static final int tableRow1=0x7f0a0015;
-        public static final int titreLivre=0x7f0a0018;
+        public static final int ACodifier=0x7f0a000f;
+        public static final int AjoutDocument=0x7f0a000d;
+        public static final int Archives=0x7f0a0011;
+        public static final int AutresActions=0x7f0a0010;
+        public static final int CreationDossier=0x7f0a000c;
+        public static final int Deconnexion=0x7f0a0008;
+        public static final int GererVosDossier=0x7f0a000b;
+        public static final int Hospitalisation=0x7f0a000a;
+        public static final int HostAnd=0x7f0a0013;
+        public static final int MonCompte=0x7f0a0012;
+        public static final int NomMedecin=0x7f0a0007;
+        public static final int Transfert=0x7f0a000e;
+        public static final int TrouverPatient=0x7f0a0009;
+        public static final int action_recherche=0x7f0a001c;
+        public static final int action_settings=0x7f0a001b;
+        public static final int ajout_consult=0x7f0a0021;
+        public static final int ajout_doc=0x7f0a0022;
+        public static final int ajout_hospi=0x7f0a0025;
+        public static final int archives=0x7f0a001f;
+        public static final int bouton_connexion=0x7f0a0004;
+        public static final int codification=0x7f0a0020;
+        public static final int encadre=0x7f0a0001;
+        public static final int grand_logo=0x7f0a0000;
+        public static final int hospi_en_cours=0x7f0a001e;
+        public static final int imageView1=0x7f0a0019;
+        public static final int infos_generales=0x7f0a001d;
+        public static final int linearLayout1=0x7f0a0005;
+        public static final int linearLayout3=0x7f0a0015;
+        public static final int linearLayout5=0x7f0a0017;
+        public static final int list=0x7f0a0018;
+        public static final int modif_data=0x7f0a0024;
+        public static final int pseudo=0x7f0a0002;
+        public static final int pswd=0x7f0a0003;
+        public static final int saveLife=0x7f0a0014;
+        public static final int slideMenu=0x7f0a0006;
+        public static final int tableRow1=0x7f0a0016;
+        public static final int titreLivre=0x7f0a001a;
+        public static final int vue_patient=0x7f0a0023;
     }
     public static final class layout {
-        public static final int activity_connexion=0x7f030000;
-        public static final int activity_hospitalisations=0x7f030001;
-        public static final int activity_recherche=0x7f030002;
-        public static final int patients_list_entry=0x7f030003;
+        public static final int activity_archives=0x7f030000;
+        public static final int activity_codification=0x7f030001;
+        public static final int activity_connexion=0x7f030002;
+        public static final int activity_hospi_en_cours=0x7f030003;
+        public static final int activity_hospitalisations=0x7f030004;
+        public static final int activity_recherche=0x7f030005;
+        public static final int activity_vue_donnees_socio=0x7f030006;
+        public static final int patients_list_entry=0x7f030007;
     }
     public static final class menu {
         public static final int accueil=0x7f090000;
-        public static final int connexion=0x7f090001;
-        public static final int lister_patients=0x7f090002;
-        public static final int recherche=0x7f090003;
+        public static final int archives=0x7f090001;
+        public static final int codification=0x7f090002;
+        public static final int connexion=0x7f090003;
+        public static final int hospi_en_cours=0x7f090004;
+        public static final int hospitalisations=0x7f090005;
+        public static final int lister_patients=0x7f090006;
+        public static final int recherche=0x7f090007;
+        public static final int vue_donnees_socio=0x7f090008;
     }
     public static final class string {
         public static final int Acodifier=0x7f06000c;
@@ -102,13 +127,33 @@ public final class R {
         public static final int VisDMP=0x7f060017;
         public static final int action_recherche=0x7f060003;
         public static final int action_settings=0x7f060001;
+        public static final int ajout_consult=0x7f06002b;
+        public static final int ajout_doc=0x7f06002c;
+        public static final int ajout_hospi=0x7f06002e;
         public static final int app_name=0x7f060000;
+        public static final int archives=0x7f060028;
+        public static final int bouton_connexion=0x7f060025;
+        public static final int codification=0x7f060029;
+        public static final int diag_principal=0x7f06002f;
+        public static final int encadre=0x7f060024;
+        public static final int grand_logo=0x7f060023;
         public static final int hello_world=0x7f060002;
+        public static final int hospi_en_cours=0x7f060027;
+        public static final int infos_generales=0x7f060026;
+        public static final int modif_data=0x7f06002d;
+        public static final int pseudo=0x7f060021;
+        public static final int pswd=0x7f060022;
         public static final int search_hint=0x7f060004;
+        public static final int title_activity_archives=0x7f060031;
+        public static final int title_activity_codification=0x7f060032;
         public static final int title_activity_connexion=0x7f060007;
+        public static final int title_activity_hospi_en_cours=0x7f060030;
+        public static final int title_activity_hospitalisations_fragment=0x7f060020;
         public static final int title_activity_lister_patients_fragment=0x7f060006;
         public static final int title_activity_recherche=0x7f060005;
+        public static final int title_activity_vue_donnees_socio=0x7f060033;
         public static final int transfert=0x7f06000b;
+        public static final int vue_patient=0x7f06002a;
     }
     public static final class style {
         /** 
--- Ouroboros/src/epf/domethic/ouroboros/activity/HospitalisationsActivity.java
+++ Ouroboros/src/epf/domethic/ouroboros/activity/HospitalisationsActivity.java
@@ -1,6 +1,5 @@
 package epf.domethic.ouroboros.activity;
 
-
 import java.util.List;
 
 import epf.domethic.ouroboros.R;
@@ -11,7 +10,6 @@ import epf.domethic.ouroboros.model.Patient;
 import android.os.Bundle;
 import android.app.ActionBar;
 import android.app.Activity;
-import android.app.ListActivity;
 import android.app.SearchManager;
 import android.content.Context;
 import android.graphics.drawable.ColorDrawable;
@@ -25,74 +23,87 @@ import android.widget.SearchView;
 
 public class HospitalisationsActivity extends Activity {
 	/** Called when the activity is first created. */
-	
-	//The menu configuration
-    private LinearLayout MenuList; //The layout where the menu is written in activity_main.xml
-    private int screenWidth;//The size of the screen
-    private boolean isExpanded;//The configuration of the menu: collapsed or expanded
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-    	
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hospitalisations);
-        ColorDrawable colorDrawable =new ColorDrawable();
-        ActionBar actionBar = getActionBar();
-        colorDrawable.setColor(0xff7184fa);
-        actionBar.setBackgroundDrawable(colorDrawable);
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        
-        actionBar.setHomeButtonEnabled(true); // The icone_launcher will not go back automatically to home. (API min 14)
-        
-        MenuList = (LinearLayout) findViewById(R.id.slideMenu);//rely the menu to the corresponding layout
-        
-        //get screen size
-        DisplayMetrics metrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(metrics);
-        screenWidth = metrics.widthPixels;//Get the horizontal size of the screen who uses the application
-                
-        List<Patient> patients = Patient.ALL;
-        ListView liste = (ListView) findViewById(R.id.list);
-        PatientAdapter aa = 
-        new PatientAdapter(this, patients);
-        liste.setAdapter(aa);       
+	// The menu configuration
+	private LinearLayout MenuList; // The layout where the menu is written in
+									// activity_main.xml
+	private int screenWidth;// The size of the screen
+	private boolean isExpanded;// The configuration of the menu: collapsed or
+								// expanded
 
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
 
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_hospitalisations);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-    	 // Inflate the options menu from XML
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.accueil, menu);
+		ColorDrawable colorDrawable = new ColorDrawable();
+		ActionBar actionBar = getActionBar();
+		colorDrawable.setColor(0xff7184fa);
+		actionBar.setBackgroundDrawable(colorDrawable);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-        // Get the SearchView and set the searchable configuration
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView = (SearchView) menu.findItem(R.id.action_recherche).getActionView();
-        // Assumes current activity is the searchable activity
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+		actionBar.setHomeButtonEnabled(true); // The icone_launcher will not go
+												// back automatically to home.
+												// (API min 14)
 
-        return true;
-    }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem menuItem){
-    	switch (menuItem.getItemId()){
-    	case android.R.id.home:// When the user click on the icone_launcher
-    		if (isExpanded) {// If the menu is visible
-                isExpanded = false;
-                //0,12 corresponds to the size of the menu compare to the screen
-                //Move MenuList from 12% of the screenwidth to 0 in 20.
-                MenuList.startAnimation(new CollapseAnimation(MenuList, 0,(int)(screenWidth*0.12), 20)); 
-            }else {// If the menu is invisible
-                isExpanded = true;
-                //Move MenuList from 0 to 12% of the screenwidth in 20.
-                MenuList.startAnimation(new ExpandAnimation(MenuList, 0,(int)(screenWidth*0.12), 20));
-            }
-    		return true;
-    	}
-    	return(super.onOptionsItemSelected(menuItem));
-    }
-    
+		MenuList = (LinearLayout) findViewById(R.id.slideMenu);// rely the menu
+																// to the
+																// corresponding
+																// layout
+
+		// get screen size
+		DisplayMetrics metrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(metrics);
+		screenWidth = metrics.widthPixels;// Get the horizontal size of the
+											// screen who uses the application
+
+		List<Patient> patients = Patient.ALL;
+		ListView liste = (ListView) findViewById(R.id.list);
+		PatientAdapter aa = new PatientAdapter(this, patients);
+		liste.setAdapter(aa);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the options menu from XML
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.accueil, menu);
+
+		// Get the SearchView and set the searchable configuration
+		SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+		SearchView searchView = (SearchView) menu.findItem(
+				R.id.action_recherche).getActionView();
+		// Assumes current activity is the searchable activity
+		searchView.setSearchableInfo(searchManager
+				.getSearchableInfo(getComponentName()));
+		searchView.setIconifiedByDefault(false); // Do not iconify the widget;
+													// expand it by default
+
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem menuItem) {
+		switch (menuItem.getItemId()) {
+		case android.R.id.home:// When the user click on the icone_launcher
+			if (isExpanded) {// If the menu is visible
+				isExpanded = false;
+				// 0,12 corresponds to the size of the menu compare to the
+				// screen
+				// Move MenuList from 12% of the screenwidth to 0 in 20.
+				MenuList.startAnimation(new CollapseAnimation(MenuList, 0,
+						(int) (screenWidth * 0.12), 20));
+			} else {// If the menu is invisible
+				isExpanded = true;
+				// Move MenuList from 0 to 12% of the screenwidth in 20.
+				MenuList.startAnimation(new ExpandAnimation(MenuList, 0,
+						(int) (screenWidth * 0.12), 20));
+			}
+			return true;
+		}
+		return (super.onOptionsItemSelected(menuItem));
+	}
+
 }
