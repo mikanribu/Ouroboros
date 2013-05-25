6cb19ebd8d1e2ed3929cf756c164ecd7bf105381
mikanribu <clementine.pages@gmail.com>
2013-05-25 11:37:19 +0200

    Modification AfficherPatientFragment

--- Ouroboros/src/epf/domethic/ouroboros/activity/AfficherPatientFragment.java
+++ Ouroboros/src/epf/domethic/ouroboros/activity/AfficherPatientFragment.java
@@ -6,9 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class AfficherPatientFragment extends Fragment {
 
+	private TextView nNomPatient;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState ){
 		View view = inflater.inflate(R.layout.fragment_visualiser_patients,container, false);
