7add9c68bd10f7616ea10d02b6b321cc2f3b15e0
mikanribu <clementine.pages@gmail.com>
2013-06-22 14:36:26 +0200

    Problème id résolu

--- Ouroboros/src/epf/domethic/ouroboros/activity/ListerPatientsFragment.java
+++ Ouroboros/src/epf/domethic/ouroboros/activity/ListerPatientsFragment.java
@@ -49,8 +49,7 @@ public class ListerPatientsFragment extends SherlockListFragment {
 	private PatientDAO dao;
 	private Patient patient; // Création d'un patient de la classe Patient
 
-	private final static String TAG = ListerPatientsFragment.class
-			.getSimpleName();
+	private final static String TAG = ListerPatientsFragment.class.getSimpleName();
 	private PatientCursorAdapter adapter; // Création du Cursor pour les
 											// patients via la Base de Données
 
@@ -177,7 +176,6 @@ public class ListerPatientsFragment extends SherlockListFragment {
 				JSONObject c = patients.getJSONObject(i);
 
 				// On récupère toutes les données qu'on stocke dans une variable
-				int id = c.getInt(PatientColumns._ID);
 				String nom = c.getString(PatientColumns.KEY_NOM);
 				String prenom = c.getString(PatientColumns.KEY_PRENOM);
 				Sexe sexe = Sexe.valueOf(c.getString(PatientColumns.KEY_SEXE));
@@ -203,7 +201,7 @@ public class ListerPatientsFragment extends SherlockListFragment {
 						.getString(PatientColumns.KEY_MEDECIN_TRAITANT);
 				boolean hospitalise = c.getInt(PatientColumns.KEY_HOSPITALISE) == 1;
 				// Création d'un patient avec les données
-				Patient p = new Patient(id, nom, prenom, sexe, dateNaissance,
+				Patient p = new Patient(nom, prenom, sexe, dateNaissance,
 						lieuNaissance, adresse, ville, codePostal, pays,
 						nationalite, telephone, numSS, medecinTraitant,
 						hospitalise);
